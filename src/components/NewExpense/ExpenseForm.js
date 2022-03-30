332db179cbf1b51481670ba31a0e5d80484f520c
dhanush57 <dhanushreddy57@gmail.com>
2022-03-30 21:54:28 +0530

    Initial commit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -58,7 +58,7 @@ function ExpenseForm(props) {
           <label>Date</label>
           <input
             type="date"
-            min="2019-01-01"
+            min="2014-01-01"
             max="2022-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
